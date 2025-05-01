8d055660399d109105be8f8ced6c8ab4d03e1666
ShanT237 <santiago.rodriguezt@uqvirtual.edu.co>
2025-04-30 21:18:38 -0500

    CODE1.6

--- Clinica-main/src/main/java/module-info.java
+++ Clinica-main/src/main/java/module-info.java
@@ -6,8 +6,6 @@ module co.edu.uniquindio.clinica {
     requires org.simplejavamail.core;
 
 
-    opens co.edu.uniquindio.clinica to javafx.fxml;
-    exports co.edu.uniquindio.clinica;
     exports co.edu.uniquindio.clinica.controladores;
     opens co.edu.uniquindio.clinica.controladores to javafx.fxml;
     exports co.edu.uniquindio.clinica.app;
