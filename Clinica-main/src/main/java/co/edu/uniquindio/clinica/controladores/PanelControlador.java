4d2183a7dc37b620007457f67132bf14674908e9
ShanT237 <santiago.rodriguezt@uqvirtual.edu.co>
2025-05-01 21:35:19 -0500

    CODE1.7

--- Clinica-main/src/main/java/co/edu/uniquindio/clinica/controladores/PanelControlador.java
+++ Clinica-main/src/main/java/co/edu/uniquindio/clinica/controladores/PanelControlador.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -13,7 +14,7 @@ import javafx.scene.control.TabPane;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class PanelControlador {
+public class PanelControlador implements Initializable {
 
     @FXML
     private ResourceBundle resources;
@@ -66,14 +67,16 @@ public class PanelControlador {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(rutaFXML));
             Parent root = loader.load();
+            tab.setContent(root);
 
-            // Solo cambia el contenido de la pestaña, no creamos una nueva ventana
-            if (tab.getContent() == null) {
-                tab.setContent(root);
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        configuracionVentanas();
     }
 }
 
